f9a820069fbbbf7f5b8ae719056fdd6623e60324
Edvin Oscarsson Ferner <edvin.oskarsson.ferner@b3.se>
2024-09-16 20:25:00 +0200

    added initial test python file

---
+++ test.py
@@ -0,0 +1,15 @@
+def run():
+    x:str= 14;
+
+    y = "hej"
+
+    print (x);
+    print('hej'.upper())
+
+    min_variabel = 3
+    print(min_variabel)
+
+
+
+if __name__ == "__main__":
+    run()
